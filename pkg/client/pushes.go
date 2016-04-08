9e4c8b3edff4d8edf61431ac821886f9ab10de04
pi-victor <ipalade@redhat.com>
2016-04-08 19:30:39 +0200

    add rudimentary listing of pushes

--- pkg/client/pushes.go
+++ pkg/client/pushes.go
@@ -1,19 +1,28 @@
 package client
 
 import (
-	"github.com/PI-Victor/gopushbullet/pkg/log"
-	"github.com/PI-Victor/gopushbullet/pkg/util"
+	"encoding/json"
+
+	"github.com/PI-Victor/gunner/pkg/log"
+	"github.com/PI-Victor/gunner/pkg/util"
 )
+
+// PushesList contains the data associated with all the retrieved pushes
+type PushesList struct {
+	Pushes []PushURL `json:"pushes"`
+	Cursor string    `json:"cursor"`
+}
 
 // PushURL is the structure for pushes
 type PushURL struct {
 	Active        bool    `json:"active"`
 	Body          string  `json:"body"`
+	URL           string  `json:"url"`
 	Created       float64 `json:"created"`
 	Direction     string  `json:"direction"`
-	Dismissed     string  `json:"dismissed"`
-	Ident         string  `json:"ident"`
-	Modified      string  `json:"modified"`
+	Dismissed     bool    `json:"dismissed"`
+	Ident         string  `json:"iden"`
+	Modified      float64 `json:"modified"`
 	ReceiverEmail string  `json:"receiver_email_normalized"`
 	ReceiverID    string  `json:"receiver_iden"`
 	SenderEmail   string  `json:"sender_email_normalized"`
@@ -25,14 +34,44 @@ type PushURL struct {
 
 // ListPushes retrieves pushes from an account and stores them on disk
 func ListPushes() {
-	newUserDetails := UserDetails{}
+	userDetails := UserDetails{}
 	newConfig := NewConfig()
-	newConfig.ReadConfig(newUserDetails)
-	log.Debug("printing", newUserDetails)
+	configDetails, err := newConfig.ReadConfig()
+	if err != nil {
+		log.Fatal("", err)
+	}
+	err = json.Unmarshal(configDetails, &userDetails)
+	if err != nil {
+		log.Fatal("", err)
+	}
+	// create a bogus request
+	headerRequestOpt := make(map[string]string)
 
-	headerRequestOpt := map[string]string{
-		"deleted": "off",
+	response, err := util.ProcessAPIRequest("GET", util.PushesAPIURL, userDetails.Token, headerRequestOpt)
+
+	if err != nil {
+		log.Fatal("", err)
 	}
 
-	util.ProcessAPIRequest("GET", util.PushesAPIURL, newUserDetails.Token, headerRequestOpt)
+	pushes := &PushesList{}
+	err = json.Unmarshal(response, &pushes)
+	if err != nil {
+		log.Fatal("", err)
+	}
+
+	if pushes.Cursor != "" {
+		log.Debug("Cursor is not empty", pushes.Cursor)
+	}
+
+	for _, i := range pushes.Pushes {
+		if i.Type == "" {
+			log.Debug("This is not an URL: %#v", i)
+			continue
+		}
+		log.Info("%+v", i.URL)
+	}
+}
+
+func pushPrinter() {
+
 }
